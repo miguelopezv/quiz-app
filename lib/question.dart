e33f109fca1bea17a897362ca34c1baa51d706e4
miguelopezv <miguelopezv@gmail.com>
2020-05-09 11:49:48 -0500

    refactor question constructor

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,5 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question(string, bool) {
-    questionText = string;
-    questionAnswer = bool;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
